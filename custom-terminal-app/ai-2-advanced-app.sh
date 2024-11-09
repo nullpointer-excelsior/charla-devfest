01dcd5a27faf2a7a1c562b92610e65728144c95b
Benjamin Team Rocket <benjamin@team.rocket.com>
2024-11-09 10:40:48 -0300

    new examples

--- custom-terminal-app/ai-2-advanced-app.sh
+++ custom-terminal-app/ai-2-advanced-app.sh
@@ -7,7 +7,13 @@ fabric --help
 fabric -l
 
 # uso de un pattern
-pbpaste | fabric --pattern summarize
+cat pokemon-tale.txt | fabric --pattern summarize --language es --stream
+
+# variedad de opciones nos permite una experiencia llena de creatividad con ia
+cat pokemon-tale.txt | fabric --pattern analyze_personality --language es --stream
+
+# pipes nos permite tareas complejas
+cat pokemon-tale.txt | fabric --pattern analyze_personality --language es | python ai translate
 
 # usamos una caracteristica interesante con youtube
 fabric -y "https://www.youtube.com/watch?v=v81EsjzxWFo&t=461s" --stream --pattern extract_wisdom -g es
